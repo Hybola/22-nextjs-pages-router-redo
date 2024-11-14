f9cfb9d9742e7abc8ff8448ca54c927560fd2065
Hybola <keeratipong.b@gmail.com>
2024-11-14 14:53:42 +0700

    lecture 499

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
+      <Header />
+      <div>
+        <main className="max-w-7xl mx-auto w-full">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </div>
+  );
 }
