4bac42a2e2d5510a3907233fe3e85a6edcff235b
Hybola <keeratipong.b@gmail.com>
2024-11-14 15:17:10 +0700

    lecture 502

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,14 +1,22 @@
+import CabinView from "@/components/CabinView";
+import { getCabin } from "@/lib/data-service";
 import Head from "next/head";
 import { useRouter } from "next/router";
+export async function getServerSideProps({ params }) {
+  const cabin = await getCabin(params.cabinId);
+  return { props: { cabin } };
+}
 
-function Cabin() {
+function Cabin({ cabin }) {
   const router = useRouter();
   return (
     <>
       <Head>
-        <title>Cabin {router.query.cabinId}//The Wild Oasis</title>
+        <title>Cabin {cabin.name}//The Wild Oasis</title>
       </Head>
-      <div>Cabin #{router.query.cabinId}</div>
+      <div className="max-w-6xl mx-auto mt-8">
+        <CabinView cabin={cabin} />
+      </div>
     </>
   );
 }
