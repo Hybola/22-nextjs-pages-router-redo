a069e4d9e758235ca89fe505339ef3672c06ba19
Hybola <keeratipong.b@gmail.com>
2024-11-14 14:59:10 +0700

    Lecture 500

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Head from "next/head";
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
@@ -8,14 +9,20 @@ const josefin = Josefin_Sans({
 
 export default function App({ Component, pageProps }) {
   return (
-    <div
-      className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
-      <Header />
-      <div>
-        <main className="max-w-7xl mx-auto w-full">
-          <Component {...pageProps} />
-        </main>
+    <>
+      <Head>
+        <title>The Wild Oasis</title>
+        <link rel="icon" href="logo.png" />
+      </Head>
+      <div
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
+        <Header />
+        <div>
+          <main className="max-w-7xl mx-auto w-full">
+            <Component {...pageProps} />
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
