0f4d514ebfb551af98ea414f0c412f4aa1419c45
Hybola <keeratipong.b@gmail.com>
2024-11-14 15:32:25 +0700

    lecture 503

---
+++ pages/api/contact.js
@@ -0,0 +1,26 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({
+      success: false,
+      message: "405: method is not allowed. Please make a POST request",
+    });
+  const contactData = {
+    fullName: "Jinny",
+    email: "jinny@test.com",
+    subject: "booking",
+    message: "Hai",
+  };
+  const { error } = await supabase.from("contact").insert([contactData]);
+  if (error)
+    return res.status(500).json({
+      success: false,
+      message: "Could not send your message, please try again",
+    });
+
+  res.status(200).json({
+    success: true,
+    message: "Thanks for your message! We will be in touch soon:)",
+  });
+}
